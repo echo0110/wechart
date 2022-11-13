05b8b3644e15ee20ea2ee2323fc4c77102352a93
niuben <820713556@qq.com.com>
2022-11-13 12:47:40 +0800

    WeChat applet test

---
+++ pages/test/test.js
@@ -0,0 +1,63 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    // text:"这是一个页面"
+    inputShowed: false,  //初始文本框不显示内容
+  },
+  // 事件处理函数
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad() {
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
+    }
+  },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+  },
+  getUserInfo(e) {
+    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+    console.log(e)
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  onShow() {
+
+    console.log(app.globalData.userInfo);
+    console.log(app.globalData.openid);
+
+
+  },
+   // 使文本框进入可编辑状态
+   showInput: function () {
+    this.setData({
+      inputShowed: true   //设置文本框可以输入内容
+    });
+  },
+  // 取消搜索
+  hideInput: function () {
+    this.setData({
+      inputShowed: false
+    });
+  }
+})
